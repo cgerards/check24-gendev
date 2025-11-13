dcd11420224adbcfafc4a1f9e2b90ba1dd49cf92
Christoph Alexander Gerards <christoph.gerards@tum.de>
2025-11-13 16:47:03 +0100

    Navbar and first widget experiment

--- check24-web/src/app/page.tsx
+++ check24-web/src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import BasicExample from "../components/Navbar";
-
+import Carousel from "../widgets/Carousel";
 
 export default function Home() {
   return (
     <div>
       <BasicExample />
       <p>Test</p>
+      <div className="px-30 py-10">
+        <Carousel></Carousel>
+      </div>
     </div>
   );
 }
--- check24-web/src/components/Navbar.tsx
+++ check24-web/src/components/Navbar.tsx
@@ -1,17 +1,20 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
+import mainLogo from "../../public/check24.svg";
 
 function BasicExample() {
   return (
-    <nav className="bg-checkblue border-b py-4">
-      <div className="max-w-8xl mx-auto px-1 sm:px-2 lg:px-4">
-        <div className="flex justify-between h-16 items-start">
+    <nav className="bg-checkblue border-b pt-6">
+      <div className="max-w-8xl mx-auto px-1 sm:px-2 lg:px-10">
+        <div className="flex justify-between h-12 items-start">
           <div className="flex items-center">
-            <Link href="/" className="text-xl font-bold text-white">
-              MySite
-            </Link>
-            <div className='text-white ml-4'>Test</div>
+            <div className="container">
+              <Link href="#">
+                <Image className="pl-3" src={mainLogo} alt="CHECK24" />
+              </Link>
+            </div>
           </div>
 
           <div className="hidden md:flex space-x-4">
@@ -31,6 +34,25 @@ function BasicExample() {
               Contact
             </Link>
           </div>
+        </div>
+        
+        
+        <div className="flex text-white text-sm font-semibold">
+          <div className="hover:bg-sky-700 py-3 px-3">Verischerungen</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Konto & Kredit</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Strom & Gas</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Internet</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Handy</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Reise</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Flüge</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Hotel & Ferienwohnung</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Mietwagen</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Profis</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Reifen</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Möbel</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Technik</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Steuer</div>
+          <div className="hover:bg-sky-700 py-3 px-3">Fußball</div>
         </div>
       </div>
     </nav>
