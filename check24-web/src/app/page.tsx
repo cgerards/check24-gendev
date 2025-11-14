ad33012b0c88c27582dbac0cd9762f34f4a107d5
Christoph Alexander Gerards <christoph.gerards@tum.de>
2025-11-14 23:14:40 +0100

    Structural changes frontpage

--- check24-web/src/app/page.tsx
+++ check24-web/src/app/page.tsx
@@ -9,13 +9,19 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen w-full">
       <CheckNavbar />
-      <main className="grow">
+      {/* 1. Use flex-col to stack children vertically. 
+         2. Use items-center to horizontally center the children. 
+         3. Add a gap for spacing between sections. */}
         <Hero />
-        <div className="px-30 py-10">
-          <p>Test</p>
-          <Carousel></Carousel>
-          <FeaturedGrid></FeaturedGrid>
+      <main className="flex flex-col grow items-center gap-y-12 py-12">
+        
+        <div className="w-full max-w-7xl px-4">
+          <Carousel />
         </div>
+        <div className="w-full max-w-5xl px-4">
+          <FeaturedGrid />
+        </div>
+
       </main>
       <Footer />
     </div>
